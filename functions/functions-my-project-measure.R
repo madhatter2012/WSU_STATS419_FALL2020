cca0a86324c43d2fafabd41f996671d554de806a
Michael Smith <madhatter2012@gmail.com>
2020-11-10 16:40:36 -0800

    my project functions update

--- functions/functions-my-project-measure.R
+++ functions/functions-my-project-measure.R
@@ -1,1 +1,19 @@
 # My Project Measure Functions
+
+
+prepareMeasureData <- function(x)
+{
+  # Removing NA values
+  x <- na.omit(x);
+  
+  # Generating Standard Deviation Scores
+  
+  
+  
+  # Correlations between Height SDS and HC SDS
+  
+  # Regression Analysis w/ HC SDS as dependent variable
+  
+  # Equations
+  
+}
